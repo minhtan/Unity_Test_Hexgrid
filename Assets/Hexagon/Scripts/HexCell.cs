72309dc6f3f68e41317367dbf1a84ac4adadefd4
Tan <m.tan2007@gmail.com>
2016-04-11 18:15:58 +0700

    init commit

---
+++ Assets/Hexagon/Scripts/HexCell.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HexCell : MonoBehaviour {
+
+}
+
+[System.Serializable]
+public struct HexCoordinates {
+
+	public int X { get; private set; }
+
+	public int Z { get; private set; }
+
+	public HexCoordinates (int x, int z) {
+		X = x;
+		Z = z;
+	}
+}
