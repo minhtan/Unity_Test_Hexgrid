2f8e158021e88d9f4a3ed1bf138133d5f4c67b30
Tan <m.tan2007@gmail.com>
2016-04-21 18:03:19 +0700

    did something..

--- Assets/HexagonTry3(MyOwn)/Script/HexCell.cs
+++ Assets/HexagonTry3(MyOwn)/Script/HexCell.cs
@@ -2,10 +2,28 @@
 using System.Collections;
 
 namespace Tan{
+	[ExecuteInEditMode]
+	[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
 	public class HexCell : MonoBehaviour {
 		CellStatus status;
+		Mesh hexMesh;
+		public Texture texture;
+
 		public HexCell(CellStatus status){
 			this.status = status;
+		}
+
+		void Start(){
+			GetComponent<MeshFilter>().mesh = hexMesh = new Mesh();
+			hexMesh.name = "Hex Mesh";
+
+			hexMesh.vertices = HexData.Vertices;
+			hexMesh.triangles = HexData.Triangles;
+
+			hexMesh.uv = HexData.Uv;
+
+			hexMesh.RecalculateNormals ();
+			GetComponent<MeshRenderer> ().material.mainTexture = texture;
 		}
 	}
 }
--- Assets/HexagonTry3(MyOwn)/Script/HexData.cs
+++ Assets/HexagonTry3(MyOwn)/Script/HexData.cs
@@ -34,5 +34,39 @@ namespace Tan{
 		public static float InnerR {
 			get {return innerR;}
 		}
+
+		static Vector3[] vertices = new Vector3[]{
+			new Vector3(0f, 0f, OuterR),
+			new Vector3(InnerR, 0f, 0.5f * OuterR),
+			new Vector3(InnerR, 0f, -0.5f * OuterR),
+			new Vector3(0f, 0f, -OuterR),
+			new Vector3(-InnerR, 0f, -0.5f * OuterR),
+			new Vector3(-InnerR, 0f, 0.5f * OuterR)
+		};
+		public static Vector3[] Vertices {
+			get {return vertices;}
+		}
+
+		static int[] triangles = new int[] {
+			0,1,2,
+			0,2,5,
+			5,2,3,
+			5,3,4
+		};
+		public static int[] Triangles {
+			get {return triangles;}
+		}
+
+		static Vector2[] uv = new Vector2[] {
+			new Vector2(0.5f, 1f),
+			new Vector2(1f, 0.75f),
+			new Vector2(1f, 0.25f),
+			new Vector2(0.5f, 0f),
+			new Vector2(0f, 0.25f),
+			new Vector2(0f, 0.75f),
+		};
+		public static Vector2[] Uv {
+			get {return uv;}
+		}
 	}
 }
